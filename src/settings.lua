96de4f4f455d06f9e39075f61c9747f6ebe2f9ad
Theodore Alteneder <theodore.alteneder@gmail.com>
2021-02-05 01:17:55 -0800

    Add mod settings for defect rates

---
+++ src/settings.lua
@@ -0,0 +1,29 @@
+data:extend({
+	{
+		type = "int-setting",
+		order = "1",
+		name = "defective-defect-rate-simple",
+		setting_type = "startup",
+		default_value = 10,
+		minimum_value = 0,
+		maximum_value = 100
+	},
+	{
+		type = "int-setting",
+		order = "2",
+		name = "defective-defect-rate-advanced",
+		setting_type = "startup",
+		default_value = 20,
+		minimum_value = 0,
+		maximum_value = 100
+	},
+	{
+		type = "int-setting",
+		order = "3",
+		name = "defective-defect-rate-complex",
+		setting_type = "startup",
+		default_value = 30,
+		minimum_value = 0,
+		maximum_value = 100
+	},
+})
